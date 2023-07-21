9bee3611e2fa25d6c7ae8c8d7ce95cfe96ebd434
kuzmavasil74 <kuzmavasil.v@gmail.com>
2023-07-21 04:26:09 +0200

    index.js_mod

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 "use strict";
-if (4 == 9) {
-  console.log(`Ok`);
-} else {
-  console.log(`Wrong`);
-}
+// if (4 == 9) {
+//   console.log(`Ok`);
+// } else {
+//   console.log(`Wrong`);
+// }
+
+// const num = 50;
+
+// if (num > 50) {
+//   console.log("Good");
+// } else if (num == 50) {
+//   console.log("Perfect");
+// } else {
+//   console.log("Bad");
+// }
+
+// const num = 50;
+// num == 50 ? console.log("Perfect") : console.log("Error");
+
+// const num = 1000;
+
+// switch (num) {
+//   case 49:
+//     console.log(" No true");
+//     break;
+//   case 100:
+//     console.log(" No true");
+//     break;
+//   case 43:
+//     console.log(" No true");
+//     break;
+//   case 50:
+//     console.log("True");
+//     break;
+//   default:
+//     console.log("Not today");
+//     break;
+// }
