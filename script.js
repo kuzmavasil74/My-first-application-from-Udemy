1c4e3958768de17f3a3ccf7e75f24b2546db6956
kuzmavasil74 <kuzmavasil.v@gmail.com>
2023-07-23 22:44:22 +0200

    cicles.js_to_script.js

--- script.js
+++ script.js
@@ -1,46 +1,82 @@
-'use strict';
+// 'use strict';
 
-// let num = 50;
-
-// while (num <= 55) {
-//   console.log(num);
-//   num++;
-// }
-
-// do {
-//   console.log(num);
-//   num++;
-// } while (num <= 55);
-// let num = 50;
-
-// for (let i = 1; i < 10; i++) {
-//   if (i === 6) {
-//     break;
-//   }
-//   console.log(i);
-// }
-
-// for (let i = 1; i <= 10; i++) {
-//   if (i === 6) {
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
 //     continue;
-//   }
-//   console.log(i);
-// }
-
-// for (let i = 0; i < 3; i++) {
-//   console.log(i);
-//   for (let j = 0; j < 3; j++) {
-//     console.log(j);
+//   } else {
+//     console.log(i);
 //   }
 // }
 
-let result = '';
-const length = 7;
+// let i = 2;
+// while (i <= 16) {
+//   if (i % 2 === 0) {
+//     i++;
+//     continue;
+//   } else {
+//     console.log(i);
+//   }
+//   i++;
+// }
 
-for (let i = 0; i < length; i++) {
-  for (let j = 0; j < i; j++) {
-    result += '*';
-  }
-  result += '\n';
-}
-console.log(result);
+// function fifthTask() {
+//   const arrayOfNumbers = [];
+//   for (let i = 5; i <= 10; i++) {
+//     arrayOfNumbers.push(i);
+//   }
+//   console.log(arrayOfNumbers);
+//   return arrayOfNumbers;
+// }
+// fifthTask();
+
+// function firstTask() {
+//   const arr = [3, 5, 8, 16, 20, 23, 50];
+//   const result = [];
+//   arr.forEach((i) => {
+//     result.push(i);
+//   });
+//   console.log(result);
+//   return result;
+// }
+// firstTask();
+
+// function secondTask() {
+//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+//   for (i = 0; i < data.length; i++) {
+//     if (typeof data[i] === 'number') {
+//       data[i] = data[i] * 2;
+//     } else if (typeof data[i] === 'string') {
+//       data[i] = `${data[i]} + ' - done'`;
+//     }
+//   }
+//   console.log(data);
+//   return data;
+// }
+// secondTask();
+
+// function thirdTask() {
+//   const data = [5, 10, 'Shopping', 20, 'Homework'];
+//   const result = [];
+//   data.reverse();
+//   data.forEach((e) => {
+//     result.push(e);
+//   });
+//   console.log(result);
+//   return result;
+// }
+// thirdTask();
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//   for (let j = 0; j < lines - i; j++) {
+//     result += ' ';
+//   }
+//   for (let j = 0; j < 2 * i + 1; j++) {
+//     result += '*';
+//   }
+//   result += '\n';
+// }
+
+// console.log(result);
